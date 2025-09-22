d9e9f8f17a0aeb804b759d414b8f22b353664880
mucalytic <mucalytic@hotmail.com>
2025-09-22 19:17:03 +0200

    Created hello world API with a CORS policy.

---
+++ Working.With.Cors.Api/Constants.cs
@@ -0,0 +1,9 @@
+namespace Working.With.Cors.Api;
+
+public static class Constants
+{
+    public static class CorsPolicyNames
+    {
+        public const string CorsPolicy = nameof(CorsPolicy); 
+    }
+}
